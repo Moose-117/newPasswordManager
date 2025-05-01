2557822a12412e6df5f4a3dab051be7a4e50475d
Moose-117 <marco.g92@hotmail.it>
2025-05-01 02:15:04 +0200

    add whatsapp recover password

--- src/main/java/com/mycompany/passwordmanager/Main.java
+++ src/main/java/com/mycompany/passwordmanager/Main.java
@@ -1,15 +1,28 @@
 package com.mycompany.passwordmanager;
 
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws NoSuchAlgorithmException {
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException, URISyntaxException {
 
 		Scanner scanner = new Scanner(System.in);
 		String choice;
+		String passwordFound = null;
 		do {
 			System.out.println("premi 1 per leggere servizio, 2 per aggiungere");
 			choice = scanner.nextLine();
@@ -18,8 +31,10 @@ public class Main {
 			case "1":
 				System.out.println("sono in leggi servizio");
 				try {
-					String searchedSubscription = "subscriptionName2";
-					ManageSubscriptions.readSubscription(searchedSubscription);
+					System.out.println("inserisci servizio da cercare");
+					String searchedSubscription = scanner.nextLine();
+					passwordFound = ManageSubscriptions.readSubscription(searchedSubscription);
+					System.out.println("password recuperata: " + passwordFound);
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -27,8 +42,31 @@ public class Main {
 				break;
 			case "2":
 				try {
-					ManageSubscriptions.addSubscription("stringa");
+					String serviceRead = scanner.nextLine();
+					String passwordRead = scanner.nextLine();
+					ManageSubscriptions.addSubscription(serviceRead, passwordRead);
 					System.out.println("sono in aggiungi servizio");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case "3":
+				try {
+					String phone = "393270904320";
+					String apikey = "1276104";
+
+					String query = String.format("phone=%s&text=%s&apikey=%s",
+							URLEncoder.encode(phone, StandardCharsets.UTF_8),
+							URLEncoder.encode(passwordFound, StandardCharsets.UTF_8),
+							URLEncoder.encode(apikey, StandardCharsets.UTF_8));
+
+					URL url = new URI("https://api.callmebot.com/whatsapp.php?" + query).toURL();
+					HttpURLConnection con = (HttpURLConnection) url.openConnection();
+					con.setRequestMethod("GET");
+
+					int responseCode = con.getResponseCode();
+					System.out.println("response code = " + responseCode);
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -38,5 +76,6 @@ public class Main {
 
 			}
 		} while (!choice.equals("exit"));
+		scanner.close();
 	}
 }
--- src/main/java/com/mycompany/passwordmanager/ManageSubscriptions.java
+++ src/main/java/com/mycompany/passwordmanager/ManageSubscriptions.java
@@ -1,25 +1,38 @@
 package com.mycompany.passwordmanager;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 public class ManageSubscriptions {
 
-	public static void addSubscription(final String stringa) throws FileNotFoundException {
-		PrintWriter writer = new PrintWriter("nuovofile.txt");
-		writer.println(stringa);
-		writer.println("The second line");
-		writer.close();
+	public static void addSubscription(final String serviceRead, final String passwordRead) throws IOException {
+
+		File file = new File("./nuovofile.txt");
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter fileWriter = new FileWriter(file, true);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		PrintWriter printWriter = new PrintWriter(bufferedWriter);
+
+		printWriter.println(serviceRead + " " + passwordRead);
+		printWriter.close();
 	}
 
 	public static String readSubscription(final String searchedSubscription) throws FileNotFoundException {
-		
+
 		String nameAndPassRow = null;
 		String subscriptionName = null;
 		String password = null;
+		String passwordRecuperata = null;
+		boolean trovato = false;
+
 		try {
 			File myObj = new File("nuovofile.txt");
 			Scanner myReader = new Scanner(myObj);
@@ -27,10 +40,12 @@ public class ManageSubscriptions {
 				nameAndPassRow = myReader.nextLine();
 				subscriptionName = nameAndPassRow.split(" ")[0];
 				password = nameAndPassRow.split(" ")[1];
-				System.out.println("subscription name" + subscriptionName);
-				System.out.println("password" + password);
-			} while((myReader.hasNextLine() && !subscriptionName.equals(searchedSubscription)));
+				trovato = subscriptionName.equals(searchedSubscription);
+				passwordRecuperata = trovato ? password : null;
+
+			} while ((myReader.hasNextLine() && !trovato));
 			myReader.close();
+			return passwordRecuperata;
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
